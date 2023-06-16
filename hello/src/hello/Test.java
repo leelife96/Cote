dcf4406fb428d7a605fbecace1810747c6e0c056
kosa <kosa@DESKTOP-ANJPP9H>
2023-06-16 16:46:42 +0900

    rra

--- hello/src/hello/Test.java
+++ hello/src/hello/Test.java
@@ -2,20 +2,24 @@ package hello;
 import java.util.Scanner;
 
 public class Test {
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	public static void main(String[] args) {
-	
-		Scanner in = new Scanner()
-	
 
+		 public static void main (String[] arg) {
+			
+			 /*int j=10;
+			 Scanner sc = new Scanner(System.in);
+		        String ch_str = sc.nextLine();
+		        String[] ch_arr = ch_str.split(" ");
+		 
+		        for (int i=j-1; 0<=i; i--)
+		            System.out.print(ch_arr[i] + " "); */
+			 
+			 int a = 50;
+		        double b = 100.12;
+		        double c = a * b;
+
+		        System.out.printf(b+" * "+a+" = "+String.format("%.0f", c));
+		    }
   }
-}
+
+
+
