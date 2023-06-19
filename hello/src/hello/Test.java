8fb65feb1642e0dfbdb8ece1c886e21ab28c62ec
kosa <kosa@DESKTOP-ANJPP9H>
2023-06-19 13:51:27 +0900

    ㅇ

--- hello/src/hello/Test.java
+++ hello/src/hello/Test.java
@@ -1,32 +1,32 @@
 package hello;
-import java.util.ArrayList;
 import java.util.Scanner;
 
-
 public class Test {
-	public static void getPrime(int num) {
-		  boolean flag;    //소수 판별을 위한 true/false
-	        int cnt = 0;     //소수의 총 개수를 확인하기 위한 변수
-	        for(int i=2; i<=num; i++){    //i는 2~num 사이에 정수
-	            flag = true;
-	            for(int j=2; j<i; j++){   //j는 i보다 작은 정수
-	                if(i%j ==0){          //i를 j로 나누었을 때, 나누어 떨어지면 소수가 아님
-	                    flag = false;
-	                }
-	            }
-	            if(flag == true){
-	                cnt++;
-	                System.out.printf(i+" "); //소수를 따로 배열에 저장하지 않고, 찾을 때 마다 출력
-	            }        
-	        }
-	        System.out.println();
-	        System.out.println("소수 개수 : " + cnt);		
-	}
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        getPrime(N);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+        
+        int[][] array = new int[9][9];
+        
+        int max = 0;
+        int a = 0, b = 0;
+        
+        for (int i = 0; i < array.length; i++) { //  array.length는 열을 의미함.
+            for (int j = 0; j < array[i].length; j++) { // array[i].length는 열을 의미함.
+                array[i][j] = sc.nextInt();  // array[i][j]에 입력 값들을 넣는다.
+                
+                if (array[i][j] > max) { // array[i][j]가 max 보다 크면 
+                    max = array[i][j]; // max에 array[i][j]를 대입한다.
+                    a = i; // 최대값이 어느 열에 있는지 찾기 위함
+                    b = j; // 최대값이 어느 행에 있는지 찾기 위함
+                }
+                
+            }
+        }
+        
+      
+        
+        System.out.println(max);
+        System.out.println((a+1) + " " + (b+1));
         
     }
-	
 }
