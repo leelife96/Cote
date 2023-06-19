e9a5cb4fefc5c273958c2d67580ed6acd76014a6
kosa <kosa@DESKTOP-ANJPP9H>
2023-06-19 14:08:23 +0900

    s

--- hello/src/hello/backjoon2566.java
+++ hello/src/hello/backjoon2566.java
@@ -1,7 +1,7 @@
 package hello;
 import java.util.Scanner;
 
-public class Test {
+public class backjoon2566 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in); 
         
@@ -25,7 +25,7 @@ public class Test {
         
       
         
-        System.out.println(max);
+        System.out.println(max); 
         System.out.println((a+1) + " " + (b+1));
         
     }
